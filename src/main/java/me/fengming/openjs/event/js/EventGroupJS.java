f30cbae33044579910974db84389acfd02ba1089
ZZZank <3410764033@qq.com>
2025-07-23 10:55:47 +0800

    one EventBusJS for one EventBus

--- src/main/java/me/fengming/openjs/event/js/EventGroupJS.java
+++ src/main/java/me/fengming/openjs/event/js/EventGroupJS.java
@@ -1,6 +1,9 @@
 package me.fengming.openjs.event.js;
 
+import me.fengming.openjs.utils.eventbus.CancellableEventBus;
 import me.fengming.openjs.utils.eventbus.EventBus;
+import me.fengming.openjs.utils.eventbus.dispatch.DispatchCancellableEventBus;
+import me.fengming.openjs.utils.eventbus.dispatch.DispatchEventBus;
 import org.mozilla.javascript.ScriptableObject;
 
 import java.util.Objects;
@@ -10,8 +13,26 @@ import java.util.Objects;
  */
 public class EventGroupJS extends ScriptableObject {
 
-    public <E> EventBusJS<E> addBus(String name, EventBus<E> bus) {
-        var busJS = new EventBusJS<>(bus);
+    public <E> EventBusJS.Regular<E> addBus(String name, EventBus<E> bus) {
+        var busJS = new EventBusJS.Regular<>(bus);
+        this.put(Objects.requireNonNull(name), this, busJS);
+        return busJS;
+    }
+
+    public <E> EventBusJS.Cancellable<E> addBus(String name, CancellableEventBus<E> bus) {
+        var busJS = new EventBusJS.Cancellable<>(bus);
+        this.put(Objects.requireNonNull(name), this, busJS);
+        return busJS;
+    }
+
+    public <E, K> EventBusJS.Dispatch<E, K> addBus(String name, DispatchEventBus<E, K> bus) {
+        var busJS = new EventBusJS.Dispatch<>(bus);
+        this.put(Objects.requireNonNull(name), this, busJS);
+        return busJS;
+    }
+
+    public <E, K> EventBusJS.DispatchCancellable<E, K> addBus(String name, DispatchCancellableEventBus<E, K> bus) {
+        var busJS = new EventBusJS.DispatchCancellable<>(bus);
         this.put(Objects.requireNonNull(name), this, busJS);
         return busJS;
     }
