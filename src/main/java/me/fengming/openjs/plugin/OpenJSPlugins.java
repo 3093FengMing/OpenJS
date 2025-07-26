9f61ce414a6b84e48f551d16bc52712ac59cfd46
ZZZank <3410764033@qq.com>
2025-07-26 19:23:03 +0800

    rewrite plugin loading

--- src/main/java/me/fengming/openjs/plugin/OpenJSPlugins.java
+++ src/main/java/me/fengming/openjs/plugin/OpenJSPlugins.java
@@ -1,12 +1,20 @@
 package me.fengming.openjs.plugin;
 
+import com.google.gson.JsonArray;
+import com.mojang.datafixers.util.Pair;
+import com.mojang.serialization.JsonOps;
 import me.fengming.openjs.OpenJS;
+import me.fengming.openjs.plugin.load.LoadablePlugin;
+import me.fengming.openjs.plugin.load.PluginLoadConditionTypeRegistryEvent;
+import me.fengming.openjs.plugin.load.condition.PluginLoadConditionType;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.forgespi.language.IModFileInfo;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 /**
  * @author FengMing
@@ -14,18 +22,29 @@ import java.util.List;
 public class OpenJSPlugins {
     public static final List<IOpenJSPlugin> plugins = new ArrayList<>();
 
-    public static void load(IModFileInfo info) {
-        Path path = info.getFile().findResource("openjs.plugins");
-        if (Files.exists(path)) {
-            try {
-                for (String l : Files.readAllLines(path)) {
-                    IOpenJSPlugin plugin = Class.forName(l).asSubclass(IOpenJSPlugin.class).getDeclaredConstructor().newInstance();
+    static {
+        MinecraftForge.EVENT_BUS.post(new PluginLoadConditionTypeRegistryEvent(PluginLoadConditionType.REGISTRY));
+    }
 
-                    plugins.add(plugin);
-                }
-            } catch (Exception e) {
-                OpenJS.LOGGER.error(e.getMessage());
+    public static void load(IModFileInfo info) {
+        Path path = info.getFile().findResource("openjs_plugins.json");
+        if (!Files.exists(path)) {
+            return;
+        }
+
+        var errorReporter = (Consumer<String>) OpenJS.LOGGER::error;
+        try (var reader = Files.newBufferedReader(path)) {
+            var json = OpenJS.GSON.fromJson(reader, JsonArray.class);
+            for (var element : json) {
+                LoadablePlugin.CODEC.decode(JsonOps.INSTANCE, element)
+                    .resultOrPartial(errorReporter)
+                    .map(Pair::getFirst)
+                    .map(LoadablePlugin::load)
+                    .flatMap(result -> result.resultOrPartial(errorReporter))
+                    .ifPresent(plugins::add);
             }
+        } catch (Exception e) {
+            errorReporter.accept(e.getMessage());
         }
     }
 }
