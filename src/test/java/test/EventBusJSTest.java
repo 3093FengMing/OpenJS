fb8e2b94920694a49bed75cd5f30c02fefbf2a29
ZZZank <3410764033@qq.com>
2025-09-27 00:59:08 +0800

    more test

--- src/test/java/test/EventBusJSTest.java
+++ src/test/java/test/EventBusJSTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mozilla.javascript.*;
 
+import java.util.function.IntConsumer;
 import java.util.function.IntSupplier;
 
 /**
@@ -13,27 +14,55 @@ import java.util.function.IntSupplier;
  */
 public class EventBusJSTest {
     private static final EventGroupJS EVENT_GROUP = new EventGroupJS("TestEvents");
-    private static final EventBusJS<IntSupplier, Void> EVENT_BUS =
-        EVENT_GROUP.createBus("supply", IntSupplier.class, true);
-
-    private static final IntSupplier EVENT = () -> 42;
+    private static final EventBusJS<IntSupplier, Void> CANCELLABLE =
+        EVENT_GROUP.createBus("cancellable", IntSupplier.class, true);
+    private static final EventBusJS<IntConsumer, Void> REGULAR =
+        EVENT_GROUP.createBus("regular", IntConsumer.class);
 
     @Test
-    public void test() {
+    public void testCancellable() {
         try (var cx = ContextFactory.getGlobal().enterContext()) {
             var scope = cx.initSafeStandardObjects();
-            ScriptableObject.putProperty(scope, "TestEvents", EVENT_GROUP);
 
-            Assertions.assertFalse(EVENT_BUS.bus().post(EVENT));
+            var binding = EVENT_GROUP.asBinding();
+            ScriptableObject.putProperty(scope, binding.name(), binding.value());
+
+            Assertions.assertFalse(CANCELLABLE.post(supplyInt(42)));
+            Assertions.assertFalse(CANCELLABLE.post(supplyInt(0)));
 
             cx.evaluateString(
                 scope, """
-                    TestEvents.supply((e) => {
-                        return true; // cancel the event
-                    });""", "test.js", 1, null
+                    TestEvents.cancellable((e) => e.getAsInt() != 0);""", "test.js", 1, null
             );
 
-            Assertions.assertTrue(EVENT_BUS.bus().post(EVENT));
+            Assertions.assertTrue(CANCELLABLE.post(supplyInt(42)));
+            Assertions.assertFalse(CANCELLABLE.post(supplyInt(0)));
+        }
+    }
+
+    private static IntSupplier supplyInt(int value) {
+        return () -> value;
+    }
+
+    @Test
+    public void testRegular() {
+        try (var cx = ContextFactory.getGlobal().enterContext()) {
+            var scope = cx.initSafeStandardObjects();
+
+            var binding = EVENT_GROUP.asBinding();
+            ScriptableObject.putProperty(scope, binding.name(), binding.value());
+
+            Assertions.assertFalse(REGULAR.post(null));
+            Assertions.assertFalse(REGULAR.post(ignored -> {}));
+
+            cx.evaluateString(
+                scope, """
+                    TestEvents.regular(e => e.accept(42));""", "test.js", 1, null
+            );
+
+            var holder = new int[1];
+            Assertions.assertFalse(REGULAR.post(value -> holder[0] = value));
+            Assertions.assertEquals(42, holder[0]);
         }
     }
 }
